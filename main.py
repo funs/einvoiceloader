7876929a1047c090b17d488bb98c102f64f8a0c1
funs <pc904b28@yahoo.com.tw>
2015-01-29 17:59:34 +0800

    Increase the ability of pass the variable from one class to another.

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager,Screen
-from kivy.properties import ListProperty, ObjectProperty
+from kivy.properties import ListProperty, ObjectProperty,StringProperty
 import einvoice
 
-class MainWidget(Screen):
+class MainScreen(Screen):
+    result_text = StringProperty('')
+    listdata = ListProperty()
     def loadcsv(self,filepath):
         print(filepath)
         self.data = einvoice.getInvoice(filepath)
@@ -17,15 +19,23 @@ class MainWidget(Screen):
     def savexls(self,filepath,data):
         print(filepath)
         einvoice.saveExcel(filepath,data)
-class TableWidget(Screen):
-        def showresult(self):
-            print(self.data)
-class MyScreenManager(ScreenManager):
-    pass
+    def change_result_text(self):
+        self.listdata = self.data
+        try:
+            for i in self.data:
+                for j in i:
+                    print(j)
+                #print('\n')
+        except Exception as err:
+            print(err)
+
+        self.result_text = str(self.listdata)
+
+class TableScreen(Screen):
+    result_text = StringProperty('')
 
 class EinvoiceApp(App):
-    def build(self):
-        return MyScreenManager()
+    pass
 
 if __name__ == "__main__":
    EinvoiceApp().run()
